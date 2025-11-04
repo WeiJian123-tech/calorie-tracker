d07170203987aea719ab4e8df51b0ced9da966e1
WeiJian123-tech <zhen16999@proton.me>
2025-11-04 10:02:23 -0500

    Refactored and added additional content to main.py to create a program that adds or tracks calories.

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
 if __name__ == "__main__":
-    print("Hello World!")
+
+    calories = 0
+
+    while True:
+        print("Press q to quit the program.")
+        print("Press any other key to continue.")
+
+        option = input("Keypress: ")
+
+        if option == "q":
+            break
+        
+        option = input("Enter the number of calories from a food item: ")
+
+        calories += int(option)
+
+        print(str(calories) + " calories added. \n")
+
+    
+    print("Number of total calories: " + str(calories))
